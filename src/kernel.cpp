9bd285d6e7a168cfedf54fec2678bf42f854fc59
Zaheer Ahmed Gujjar <zaheercena@users.noreply.github.com>
2017-06-15 15:38:10 +0500

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,6 +1,7 @@
 
 #include <common/types.h>
 #include <gdt.h>
+#include <memorymanagement.h>
 #include <hardwarecommunication/interrupts.h>
 #include <syscalls.h>
 #include <hardwarecommunication/pci.h>
@@ -9,6 +10,7 @@
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
 #include <multitasking.h>
+
 
 using namespace zaos;
 using namespace zaos::common;
@@ -154,11 +156,28 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
 
     GlobalDescriptorTable gdt;
     
+    ///Heap Malloc Instenciation
+    uint32_t* memupper = (uint32_t*)((size_t)multiboot_structure + 8);
+    size_t heap = 10*1024*1024;   ///Hard Quoted Heap size
+    MemoryManager memoryManager(heap, (*memupper)*1024 - heap - 10*1024);
+    printf("heap: 0x");
+    printfHex((heap >> 24) & 0xFF);
+    printfHex((heap >> 16) & 0xFF);
+    printfHex((heap >> 8 ) & 0xFF);
+    printfHex((heap      ) & 0xFF);
+    void* allocated = memoryManager.malloc(1024);
+    printf("\nallocated: 0x");
+    printfHex(((size_t)allocated >> 24) & 0xFF);
+    printfHex(((size_t)allocated >> 16) & 0xFF);
+    printfHex(((size_t)allocated >> 8 ) & 0xFF);
+    printfHex(((size_t)allocated      ) & 0xFF);
+    printf("\n");
+    
     TaskManager taskManager; //scheduling Multitasking Section
-    Task task1(&gdt, taskA);
+    /*Task task1(&gdt, taskA);
     Task task2(&gdt, taskB);
     taskManager.AddTask(&task1);
-    taskManager.AddTask(&task2);
+    taskManager.AddTask(&task2);*/
     
     InterruptManager interrupts(0x20, &gdt, &taskManager);
     SyscallHandler syscalls(&interrupts, 0x80);
